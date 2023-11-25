241b42bbf82afe400cd264281a6e72d0753551f3
maahi65f <144053424+maahi6@users.noreply.github.com>
2023-11-25 09:05:29 -0800

    Add files via upload

---
+++ basic.cpp
@@ -0,0 +1,10 @@
+
+
+#include<iostream>
+#include"basic.h"
+using namespace std;
+
+int main()
+{
+	cout << basic::fact(5)<< endl;
+}
---
+++ basic.h
@@ -0,0 +1,17 @@
+
+
+
+#include<iostream>
+namespace basic
+
+
+{
+	
+	int fact(int n)
+	{
+		if(n==0)
+		   return 1;
+		else
+		   return n*fact(n-1);
+   }
+}
