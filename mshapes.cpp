61e7c8a80394b6b3c57946621bee83cf1c1306b5
maahi65f <144053424+maahi6@users.noreply.github.com>
2023-11-25 10:07:30 -0800

    Add files via upload

---
+++ mshapes.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+#include"shapes.h"
+using namespace std;
+int main()
+{
+	cout<< shapes::shapes(1,1)<<endl;
+	
+	return 0;
+}
---
+++ mshapes.h
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+
+namespace shapes
+{
+	int shapes(int ,int)
+	{
+		for( int i=1;i<=5;i++)
+     	{
+	    	for(int  j=1;j<=5;j++)
+	      	{
+	        	std::cout<<"*";
+	     	}
+		std::cout<<endl;
+    	}
+	}
+}
