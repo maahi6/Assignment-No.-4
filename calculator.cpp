89c820a6649279f07d197781762c83245bc1729a
maahi65f <144053424+maahi6@users.noreply.github.com>
2023-11-25 08:15:04 -0800

    Add files via upload

---
+++ calculator.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+#include"calculator.h"
+using namespace std;
+int main()
+{
+	
+	
+	cout << calculator::add(10,20)<< endl;
+	
+	cout << calculator::sub(50,20)<< endl;
+
+	cout << calculator::mul(10,9)<< endl;
+
+	cout << calculator::div(50,5)<< endl;
+	
+	return 0;
+}
+
+
+
+
---
+++ calculator.h
@@ -0,0 +1,21 @@
+#include<iostream>
+namespace calculator
+{
+
+int add(int x , int y)
+{
+	return x + y;
+}
+int sub(int x , int y)
+{
+	return x - y;
+}
+int mul(int x , int y)
+{
+	return x * y;
+}
+int div(int x , int y)
+{
+    return x / y;
+}
+}
