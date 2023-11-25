e9aaaa561b22cdf556f0f3497902af3afc1cf4e6
maahi65f <144053424+maahi6@users.noreply.github.com>
2023-11-25 10:56:02 -0800

    Add files via upload

---
+++ mArray.h
@@ -0,0 +1,14 @@
+#include<iostream>
+#include"search.h"
+namespace searching
+{
+	int searching (){
+	
+	int array[5]={10,20,67,89,65};
+		int i,num,loc=-1;
+		
+	std::cout<<searching<<std::endl;
+}
+		
+}
+
